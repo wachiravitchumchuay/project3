cd8c6fc0356754cabfe9bc790f5fdd38d89dbcb5
pinkjo8825 <83690209+pinkjo8825@users.noreply.github.com>
2025-02-27 13:27:34 +0700

    res endpoint is work

--- src/main/java/project3/demo/RestaurantEndpoint.java
+++ src/main/java/project3/demo/RestaurantEndpoint.java
@@ -1,5 +1,19 @@
 package project3.demo;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.util.FileManager;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -7,18 +21,52 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import demo.project3.schema.GetRestaurantRequest;
 import demo.project3.schema.GetRestaurantResponse;
+import demo.project3.schema.GetRestaurantResponse.Restaurants;
 
 @Endpoint
 public class RestaurantEndpoint {
 
 	private static final String NAMESPACE_URI = "http://project3.demo/schema";
+	private static final String ONTOLOGY_FILE = "RestaurantOntology_03_12_24.rdf";
 
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getRestaurantRequest")
 	@ResponsePayload
 	public GetRestaurantResponse getRestaurant(@RequestPayload GetRestaurantRequest request) {
 		GetRestaurantResponse response = new GetRestaurantResponse();
-		response.setName(request.getName() + " Restaurant");
-		response.setLocation("123 Main St");
+		Model model = ModelFactory.createDefaultModel();
+		InputStream in = FileManager.getInternal().open(ONTOLOGY_FILE);
+		if (in == null) {
+			throw new IllegalArgumentException("File: " + ONTOLOGY_FILE + " not found");
+		}
+		model.read(in, null);
+
+		String queryString = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX re: <http://www.semanticweb.org/acer/ontologies/2567/8/restaurantontologyfinal#>
+
+SELECT ?resName
+WHERE {  
+    ?subject rdf:type re:Restaurant .
+    ?subject re:RestaurantName ?resName .
+}
+""";
+		System.out.println("Query: " + queryString);
+		Query query = QueryFactory.create(queryString);
+		try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+			ResultSet results = qexec.execSelect();
+			List<Restaurants> restaurantList = new ArrayList<>();
+			while (results.hasNext()) {
+				QuerySolution soln = results.nextSolution();
+				Literal nameLiteral = soln.getLiteral("resName");
+				String name = nameLiteral != null ? nameLiteral.getString() : "Unknown";
+				Restaurants restaurant = new Restaurants();
+				restaurant.setRestaurantName(name);
+				restaurantList.add(restaurant);
+			}
+			response.getRestaurants().addAll(restaurantList);
+		}
 		return response;
 	}
 }
--- src/main/java/project3/demo/RestaurantRecommendationEndpoint.java
+++ src/main/java/project3/demo/RestaurantRecommendationEndpoint.java
@@ -1,5 +1,23 @@
 package project3.demo;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.reasoner.Reasoner;
+import org.apache.jena.reasoner.rulesys.GenericRuleReasonerFactory;
+import org.apache.jena.util.FileManager;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.ReasonerVocabulary;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -7,18 +25,74 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import demo.project3.schema.GetRestaurantRecommendationRequest;
 import demo.project3.schema.GetRestaurantRecommendationResponse;
+import demo.project3.schema.GetRestaurantRecommendationResponse.Restaurants;
 
 @Endpoint
 public class RestaurantRecommendationEndpoint {
 
-	private static final String NAMESPACE_URI = "http://project3.demo/schema";
+    private static final String NAMESPACE_URI = "http://project3.demo/schema";
+    private static final String ONTOLOGY_FILE = "RestaurantOntology_03_12_24.rdf";
+    private static final String RULES_FILE = "rule.rules";
+    private static final String NS = "http://www.semanticweb.org/acer/ontologies/2567/8/restaurantontologyfinal#";
 
-	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getRestaurantRecommendationRequest")
-	@ResponsePayload
-	public GetRestaurantRecommendationResponse getRestaurantRecommendation(@RequestPayload GetRestaurantRecommendationRequest request) {
-		GetRestaurantRecommendationResponse response = new GetRestaurantRecommendationResponse();
-		response.setName(request.getName() + " Recommended Restaurant");
-		response.setLocation("456 Elm St");
-		return response;
-	}
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getRestaurantRecommendationRequest")
+    @ResponsePayload
+    public GetRestaurantRecommendationResponse getRestaurantRecommendation(
+            @RequestPayload GetRestaurantRecommendationRequest request) {
+        GetRestaurantRecommendationResponse response = new GetRestaurantRecommendationResponse();
+
+        // Load ontology into memory
+        OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
+        InputStream in = FileManager.getInternal().open(ONTOLOGY_FILE);
+        if (in == null) {
+            throw new IllegalArgumentException("File: " + ONTOLOGY_FILE + " not found");
+        }
+        model.read(in, null);
+
+        // Create user instance with necessary properties
+        String userURI = NS + "User_temp";
+        Resource user = model.createResource(userURI);
+        user.addProperty(RDF.type, model.createResource(userURI));
+        user.addProperty(model.createProperty(NS, "Username"), "User_temp");
+        user.addProperty(model.createProperty(NS, "PreRunProteinConsumtion"), request.getPreRunProteinConsumtion());
+        user.addProperty(model.createProperty(NS, "PostRunProteinConsumtion"), request.getPostRunProteinConsumtion());
+        user.addProperty(model.createProperty(NS, "PostRunFatConsumtion"), request.getPostRunFatConsumtion());
+        user.addProperty(model.createProperty(NS, "PostRunCarbConsumtion"), request.getPostRunCarbConsumtion());
+        user.addProperty(model.createProperty(NS, "RunnerType"), request.getRunnerType());
+        user.addProperty(model.createProperty(NS, "hasRestaurantTypeInterest"), model.createResource(NS + request.getRestaurantTypeInterest()));
+
+        // Apply rules to the model
+        Model ruleModel = ModelFactory.createDefaultModel();
+        Resource configuration = ruleModel.createResource();
+        configuration.addProperty(ReasonerVocabulary.PROPruleMode, "hybrid");
+        configuration.addProperty(ReasonerVocabulary.PROPruleSet, RULES_FILE);
+        Reasoner reasoner = GenericRuleReasonerFactory.theInstance().create(configuration);
+        Model infModel = ModelFactory.createInfModel(reasoner, model);
+
+        // Query the inference model for recommendations
+        Property hasRecommend = model.getProperty(NS, "hasRecommend");
+        Property confidence = model.getProperty(NS, "confidence");
+        Property restaurantName = model.getProperty(NS, "RestaurantName");
+
+        if (hasRecommend == null || confidence == null || restaurantName == null) {
+            throw new IllegalArgumentException("One or more properties not found in the ontology");
+        }
+
+        StmtIterator iterator = infModel.listStatements(user, hasRecommend, (RDFNode) null);
+        List<Restaurants> restaurantList = new ArrayList<>();
+        while (iterator.hasNext()) {
+            Statement stmt = iterator.nextStatement();
+            Resource recommendedRestaurant = stmt.getObject().asResource();
+            String name = recommendedRestaurant.getProperty(restaurantName).getString();
+            String conf = recommendedRestaurant.getProperty(confidence).getString();
+
+            Restaurants restaurant = new Restaurants();
+            restaurant.setRestaurantName(name);
+            restaurant.setConfidence(conf);
+            restaurantList.add(restaurant);
+        }
+        response.getRestaurants().addAll(restaurantList);
+
+        return response;
+    }
 }
